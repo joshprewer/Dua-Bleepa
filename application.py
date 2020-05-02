06ea04153ee3e8f68cfa23503190509f6787d61a
Josh Prewer <josh.prewer@focusrite.com>
2020-05-02 18:33:32 +0100

    readd model
    Former-commit-id: 69fb17ec72b3a3adf8c5c7dab53f77494b6d3a8c

--- application.py
+++ application.py
@@ -8,18 +8,18 @@ def main():
     sess = gpt2.start_tf_sess()
     if st.button('Write me a song Dua'):
         data_load_state = st.text('Loading model...')
-        # gpt2.load_gpt2(sess, run_name='run1')
+        gpt2.load_gpt2(sess, run_name='run1')
 
-        # data_load_state.text('Generating a song...')
-        # text = gpt2.generate(sess,
-        #                     prefix='<|startoftext|>',
-        #                     truncate='<|endoftext|>',
-        #                     include_prefix=False,
-        #                     return_as_list=True
-        #                     )[0]
+        data_load_state.text('Generating a song...')
+        text = gpt2.generate(sess,
+                            prefix='<|startoftext|>',
+                            truncate='<|endoftext|>',
+                            include_prefix=False,
+                            return_as_list=True
+                            )[0]
 
-        # data_load_state.text('Generated song:')
-        lyrics = st.text("blfdshajhkfdsahfkjds")
+        data_load_state.text('Generated song:')
+        lyrics = st.text(text)
 
 if __name__ == "__main__":
     main()
